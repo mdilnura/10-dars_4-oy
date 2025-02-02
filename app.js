b514852995ecfacd1eff8a243cf98d0ae7d04057
dilnura <dilnura@gmail.com>
2025-02-02 11:30:59 +0500

    initial commit: add app.js with various utility functions and index.html for script inclusion

---
+++ app.js
@@ -0,0 +1,58 @@
+// const number=[1,2,3,4,5]
+// number.forEach((n)){
+
+// }
+// let n = 5;
+// function getInitialOdds(n) {
+//   let arr = [];
+//   for (let i = 1; i <= 5; i++) {
+//     arr.push(2 * i - 1);
+//   }
+//   return arr;
+// }
+// const result = getInitialOdds(n);
+// console.log(result);
+// let arr=[4,5,7,8,6,9];
+// const getEvenReverce=(arr)=>{
+//     const arr=[];
+//     arr.forEach((item)=>{
+
+//     }
+// }
+//3========================================================
+// const arr = [4, 5, 7, 8, 6, 9];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i], arr[arr.length - 1 - i]);
+// }
+// console.log(result.tostring(" "));
+//4=========================================================
+// let arr = [1, 6, 9, 5, 8, 10, 15];
+// function rangeSum(arr, K, L) {
+//   let result = 0;
+//   for (i = K; i <= L; i++) {
+//     result += arr[i];
+//   }
+//   return result;
+// }
+// const result = rangeSum(arr, 2, 5);
+// console.log(result);
+//5555555555555555555555555555555555555555555555555555555555
+// let arr = [7, 4, 2, 3, 1, 4, 5, 2, 4, 5, 7];
+// function getSingleArr(arr) {
+//   const result = [];
+//   arr.forEach((item) => {
+//     if (!result.includes(item)) {
+//       result.push(item);
+//     }
+//   });
+//   return result;
+// }
+// const result = getSingleArr(arr);
+// console.log(result);
+// let arr = [7, 4, 9, 2, 3, 1, 5];
+// let max = arr.indexOf(Math.max(...arr));
+// let min = arr.indexOf(Math.min(...arr));
+// [arr[max], arr[min]] = [arr[min], arr[max]];
+// console.log(arr);
+ 
